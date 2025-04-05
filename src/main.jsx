cf3a7fc2b1fcae45439326be383b700901e64212
Vlad Ivannikov <vladmaslow1@gmail.com>
2025-04-05 21:35:11 +0500

    Разобрался с tailwind сделал настройки основные, добавил темы светлую и темную, сделал конфиги, в общем базовую структуру собрал

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ThemeProvider } from "./components/theme-provider";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
