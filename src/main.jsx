d0ad9768f341d0cb37035204d17bc1f006802894
Vlad Ivannikov <vladmaslow1@gmail.com>
2025-04-06 20:21:39 +0500

    обновил настройки, сделал чтобы тема переключалась через пропсы

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { ThemeProvider } from "./components/theme-provider";
+import { ThemeProvider } from "@/context/theme-provider.jsx"
+
+
 import App from './App.jsx'
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
