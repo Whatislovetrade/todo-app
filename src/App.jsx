42182cfc4f65d6f58be0b01e1b706f187dd3040e
Vlad Ivannikov <vladmaslow1@gmail.com>
2025-04-07 15:17:07 +0500

    Добавил компонент хедер и переключателем темной и светлой темы

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-function App() {    
+import TodoHeader from "./components/TodoHeader/TodoHeader";
+
+function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex justify-center items-center min-w-screen h-screen">
+      <div className="todo-app bg-white dark:bg-gray-900 dark:shadow-neutral-800 w-3xl min-h-screen rounded-[20px] shadow-2xl">
+          <TodoHeader />
+      </div>
     </div>
- 
+
   );
 }
 
---
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "../Toggle"
+
+const TodoHeader = () => {
+  return (
+    <div className="todo-header flex justify-center items-center relative">
+        <h1 className="text-center pt-10 text-[40px] font-[Inter] font-bold">To-Do List</h1>
+        <ModeToggle />
+  </div>
+  )
+}
+
+export default TodoHeader
