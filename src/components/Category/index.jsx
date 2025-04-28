69b224e8f9329db3cf30cf194e26226be72fcf1c
Vlad <vladmaslow1@gmail.com>
2025-04-28 20:24:05 +0500

    Сделал добавление и удаление элементов из списка

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -4,6 +4,8 @@ import CategoryItem from "../CategoryItem"
 import { nanoid } from "nanoid"
 import Plus from "../ui/plus"
 
+
+
 const categoryArr = [
   { id: nanoid(5), name: "All" },
   { id: nanoid(5), name: "Personal" },
@@ -15,17 +17,27 @@ const Category = () => {
   const [addCategory, setAddCategory] = useState(categoryArr)
 
   const addCategoryItem = () => {
+    let newItem = prompt('Введите название категории')
     setAddCategory((prev) => [
       ...prev,
-      { id: nanoid(5), name: 'New Item' }
-    ]);
+      { id: nanoid(5), name: newItem }
+    ]); 
+  };
+
+  const onDeleteItem = (id) => {
+    setAddCategory((prev) => {
+      if (id === prev[0].id) {
+        return prev; 
+      }
+      return prev.filter(item => item.id !== id);
+    });
   };
   
 
     return (
       <aside className="todo-category">
         <ul className="mb-[20px]">
-          <CategoryItem addCategory={addCategory} />
+          <CategoryItem addCategory={addCategory} onDelete={onDeleteItem} />
         </ul>
         
         <div
--- src/components/CategoryItem/index.jsx
+++ src/components/CategoryItem/index.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import classNames from "classnames"
+import { Trash2 } from 'lucide-react';
 
-const CategoryItem = ({ addCategory }) => {
+
+const CategoryItem = ({ addCategory, onDelete }) => {
   
   const [isActive, setIsActive] = useState(addCategory[0].id)
   
@@ -10,13 +12,20 @@ const CategoryItem = ({ addCategory }) => {
   }
 
   const elements = addCategory.map((item) => (
-    <li
-      key={item.id}
-      className={classNames('inline-block hover:bg-gray-50 w-[167px] h-[47px] text-[24px] rounded-[10px] p-2 mb-1.5 cursor-pointer dark:hover:bg-gray-800', {'bg-gray-50 hover:bg-gray-50 dark:bg-gray-800 inline-block w-[167px] h-[47px] text-[24px] rounded-[10px] p-2 mb-1.5 cursor-pointer dark:hover:bg-gray-800': isActive === item.id})}
-      onClick={() => changeState(item.id)}
-    >
-      {item.name}
-    </li>
+
+      <li
+        key={item.id}
+        className={classNames('inline-block hover:bg-gray-50 w-[167px] h-[47px] text-[24px] rounded-[10px] p-2 mb-1.5 cursor-pointer dark:hover:bg-gray-800', {'bg-gray-50 hover:bg-gray-50 dark:bg-gray-800 inline-block w-[167px] h-[47px] text-[24px] rounded-[10px] p-2 mb-1.5 cursor-pointer dark:hover:bg-gray-800': isActive === item.id})}
+        onClick={() => changeState(item.id)}
+      >
+        
+        {item.name}
+        <Trash2 
+          className="relative left-[180px] bottom-8"
+          onClick={() => onDelete(item.id)}
+        />
+
+      </li>
   ))
 
   return (
